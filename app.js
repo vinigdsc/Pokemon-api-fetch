fdb5e7cee2a3de0f12740749e72574048125eaf7
Vinicius Caetano <viniciusgcae@gmail.com>
2022-11-07 19:25:05 -0300

    add botao random

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const pic = document.getElementById("pic");
 
 const btn = document.getElementById("btn1");
 
+const btn2 = document.getElementById("btn2");
+
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   input = document.querySelector("#input");
@@ -13,15 +15,44 @@ btn.addEventListener("click", function (e) {
     })
     .then((data) => {
       //o que fazer com o Json
-      console.log(data);
-      document.getElementById("nome").innerHTML = data["name"];
-      document.getElementById("numero").innerHTML = data["id"];
+      //console.log(data);
+      document.getElementById("nome").innerHTML = `Nome: ${data["name"]}`;
+      document.getElementById("numero").innerHTML = `Numero : ${data["id"]}`;
+
+      document.getElementById("tipo").innerHTML = "tipo: ";
+      data["types"].forEach((type1) => {
+        document.getElementById("tipo").innerHTML += " " + type1.type.name;
+      });
+
       let img = data["sprites"]["front_default"];
       document.getElementById("pic").setAttribute("src", img);
     })
     .catch((erro) => {
-      console.log("Erro: " + erro);
+      document.getElementById("nome").innerHTML = "Nome/ID Invalido";
+    });
+});
+
+btn2.addEventListener("click", function (random) {
+  random.preventDefault();
+  value2 = Math.floor(Math.random() * (1000 - 1 + 1) + 1);
+  console.log(value2);
+  const url = `https://pokeapi.co/api/v2/pokemon/${value2}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("nome").innerHTML = `Nome: ${data["name"]}`;
+      document.getElementById("numero").innerHTML = `Numero: ${data["id"]}`;
+      let img = data["sprites"]["front_default"];
+      document.getElementById("pic").setAttribute("src", img);
+      document.getElementById("tipo").innerHTML = "tipo: ";
+      data["types"].forEach((type1) => {
+        document.getElementById("tipo").innerHTML += " " + type1.type.name;
+      });
     });
 });
 
 document.getElementById("btn1").onclick = load();
+
+document.getElementById("tipo").innerHTML += " " + type1.type.name;
